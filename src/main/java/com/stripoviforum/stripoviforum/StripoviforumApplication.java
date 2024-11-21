96263cb953a333c5b310aa4a746576be6a91bb74
adna-varupa <adna.varupa.22@size.ba>
2024-11-21 11:51:21 +0100

    inicijalni commit

---
+++ src/main/java/com/stripoviforum/stripoviforum/StripoviforumApplication.java
@@ -0,0 +1,13 @@
+package com.stripoviforum.stripoviforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripoviforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripoviforumApplication.class, args);
+	}
+
+}
