bc51656c3b28aa574fa3cb433f4e425104d67df8
adna-varupa <adnavarupa03@gmail.com>
2024-11-24 01:33:46 +0100

    Dodani post i strip kontroleri i njihovi html fajlovi

---
+++ src/main/java/com/stripoviforum/stripoviforum/controllers/StripoviController.java
@@ -0,0 +1,42 @@
+package com.stripoviforum.stripoviforum.controllers;
+
+
+import com.stripoviforum.stripoviforum.entities.Stripovi;
+import com.stripoviforum.stripoviforum.services.StripoviService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/comics")  // Putanja za pristup stripovima
+public class StripoviController {
+
+    @Autowired
+    private StripoviService stripoviService;  // Servis za upravljanje stripovima
+
+    // GET: Prikaz svih stripova
+    @GetMapping
+    public String listComics(Model model) {
+        List<Stripovi> comics = stripoviService.findAllComics();  // Dohvati sve stripove
+        model.addAttribute("comics", comics);  // Dodaj stripove u model
+        return "comic_list";  // Pogled za prikaz svih stripova
+    }
+
+    // GET: Prikaz detalja o jednom stripu
+    @GetMapping("/{comicId}")
+    public String viewComic(@PathVariable Long comicId, Model model) {
+        stripoviService.findComicById(comicId).ifPresent(comic -> model.addAttribute("comic", comic));  // Dohvati strip po ID-u
+        return "comic_view";  // Pogled za detalje stripa
+    }
+
+    // GET: Prikaz obrasca za dodavanje novog stripa
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("comic", new Stripovi());  // Dodaj novi prazni strip u model
+        return "comic_form";  // Pogled za obrazac za kreiranje novog stripa
+    }
+}
+// POST: Spremi no
